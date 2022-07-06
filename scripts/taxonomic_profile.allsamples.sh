e3b31982fcde8264d4097762a70cea7331b14931
jflucier <3505568+jflucier@users.noreply.github.com>
2022-07-06 09:01:09 -0400

    remove message from taxo all

--- scripts/taxonomic_profile.allsamples.sh
+++ scripts/taxonomic_profile.allsamples.sh
@@ -34,18 +34,18 @@ echo "combine all samples kreports in one"
 export __KREPORTS=$(ls $TAXONOMIC_ALL_BRACKEN_KREPORTS)
 /project/def-ilafores/common/KrakenTools/combine_kreports.py \
 -r $__KREPORTS \
--o $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME}_S.kreport \
+-o $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME}_S.kreport \
 --only-combined --no-headers
 
 echo "convert kreport to mpa"
 python /project/def-ilafores/common/KrakenTools/kreport2mpa.py \
--r $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME}_S.kreport \
--o $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME}_temp_S.MPA.TXT
+-r $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME}_S.kreport \
+-o $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME}_temp_S.MPA.TXT
 
 echo "modify mpa for humann support"
-grep "|s" $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME}_temp_S.MPA.TXT \
+grep "|s" $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME}_temp_S.MPA.TXT \
 | awk '{printf("%s\t\n", $0)}' - \
-| awk 'BEGIN{printf("#mpa_v30_CHOCOPhlAn_201901\n")}1' - > $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME}-bugs_list.MPA.TXT
+| awk 'BEGIN{printf("#mpa_v30_CHOCOPhlAn_201901\n")}1' - > $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME}-bugs_list.MPA.TXT
 
 for taxa_str in $__all_taxas
 do
@@ -63,9 +63,9 @@ do
     echo "runinng combine for $taxa_name"
     python /project/def-ilafores/common/KrakenTools/combine_mpa.py \
     -i $OUPUT_PATH/*/*_bracken/*_bracken_${taxa_oneletter}.MPA.TXT \
-    -o $OUPUT_PATH/temp_${taxa_oneletter}.tsv
+    -o $OUPUT_PATH/taxonomic_profile_all/temp_${taxa_oneletter}.tsv
 
-    sed -i "s/_bracken_${taxa_oneletter}.kreport//g" $OUPUT_PATH/temp_${taxa_oneletter}.tsv
+    sed -i "s/_bracken_${taxa_oneletter}.kreport//g" $OUPUT_PATH/taxonomic_profile_all/temp_${taxa_oneletter}.tsv
 
     if [[ ${taxa_oneletter_tmp} == "D" ]]
     then
@@ -74,7 +74,7 @@ do
         taxa_oneletter_tmp=${taxa_oneletter_tmp};
     fi
 
-    grep -E "(${taxa_oneletter_tmp:0:1}__)|(#Classification)" $OUPUT_PATH/temp_${taxa_oneletter}.tsv > $OUPUT_PATH/taxtable_${taxa_oneletter}.tsv
+    grep -E "(${taxa_oneletter_tmp:0:1}__)|(#Classification)" $OUPUT_PATH/taxonomic_profile_all/temp_${taxa_oneletter}.tsv > $OUPUT_PATH/taxonomic_profile_all/taxtable_${taxa_oneletter}.tsv
 done
 
 
@@ -82,16 +82,16 @@ source /project/def-ilafores/common/humann3/bin/activate
 export PATH=/nfs3_ib/ip29-ib/ip29/ilafores_group/programs/diamond-2.0.14/bin:$PATH
 
 ### gen python chocphlan cusotm db
-cd $OUPUT_PATH
+cd $OUPUT_PATH/taxonomic_profile_all
 echo "runnin create prescreen db. This step might take long"
-python -u ${EXE_PATH}/create_prescreen_db.py $TAXONOMIC_ALL_CHOCOPHLAN_DB $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME}-bugs_list.MPA.TXT
+python -u ${EXE_PATH}/create_prescreen_db.py $TAXONOMIC_ALL_CHOCOPHLAN_DB $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME}-bugs_list.MPA.TXT
 ### gen bowtie index on db
 mv _custom_chocophlan_database.ffn ${TAXONOMIC_ALL_NT_DBNAME}.ffn
 bowtie2-build --threads ${TAXONOMIC_ALL_SLURM_NBR_THREADS} ${TAXONOMIC_ALL_NT_DBNAME}.ffn  ${TAXONOMIC_ALL_NT_DBNAME}
 
-echo "Please move $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME} bowtie index to the location of your custom chocophlan db."
-echo "i.e. mv $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME}* /nfs3_ib/ip29-ib/ssdpool/shared/ilafores_group/humann_dbs/"
-echo "Also move $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME}-bugs_list.MPA.TXT to the location of your custom chocophlan db for reference."
-echo "i.e. mv $OUPUT_PATH/${TAXONOMIC_ALL_NT_DBNAME}-bugs_list.MPA.TXT /nfs3_ib/ip29-ib/ssdpool/shared/ilafores_group/humann_dbs/"
+# echo "Please move $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME} bowtie index to the location of your custom chocophlan db."
+# echo "i.e. mv $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME}* /nfs3_ib/ip29-ib/ssdpool/shared/ilafores_group/humann_dbs/"
+# echo "Also move $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME}-bugs_list.MPA.TXT to the location of your custom chocophlan db for reference."
+# echo "i.e. mv $OUPUT_PATH/taxonomic_profile_all/${TAXONOMIC_ALL_NT_DBNAME}-bugs_list.MPA.TXT /nfs3_ib/ip29-ib/ssdpool/shared/ilafores_group/humann_dbs/"
 
 echo "humann custom buglist db analysis completed"
