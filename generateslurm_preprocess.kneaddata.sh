53da79a73aebd24b1f1a64e870beadf7f94d5ab5
jflucier <3505568+jflucier@users.noreply.github.com>
2023-02-23 10:04:39 -0500

    kneaddata all unmatched bug correction

--- generateslurm_preprocess.kneaddata.sh
+++ generateslurm_preprocess.kneaddata.sh
@@ -194,9 +194,10 @@ do
 	echo "submitting sbatch --array=$start_i-$end_i ${out}/preprocess.kneaddata.slurm.sh"
 	sbatch --array=$start_i-$end_i '${out}'/preprocess.kneaddata.slurm.sh
 
-	echo "next submittion in $batch_time sec"
-	sleep $batch_time
-
+	if [ "$end_i" -lt "'$sample_nbr'" ]; then
+      echo "next submittion in $batch_time sec"
+      sleep $batch_time
+  fi
 done
 
 echo "done"
