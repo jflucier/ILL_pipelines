4dda639d0e737778c44751c061203f80fdf72761
jflucier <3505568+jflucier@users.noreply.github.com>
2023-03-31 13:40:34 -0400

    metaphlan dev

--- generateslurm_taxonomic_abundance.metaphlan.sh
+++ generateslurm_taxonomic_abundance.metaphlan.sh
@@ -18,8 +18,8 @@ help_message () {
   echo "   --slurm_log STR	slurm log file output directory (default to output_dir/logs)"
   echo "   --slurm_email \"your@email.com\"	Slurm email setting"
   echo "   --slurm_walltime STR	slurm requested walltime (default 24:00:00)"
-  echo "   --slurm_threads INT	slurm requested number of threads (default 24)"
-  echo "   --slurm_mem STR	slurm requested memory (default 30G)"
+  echo "   --slurm_threads INT	slurm requested number of threads (default 12)"
+  echo "   --slurm_mem STR	slurm requested memory (default 25G)"
 
   echo ""
   echo "   -h --help	Display help"
@@ -33,8 +33,8 @@ export EXE_PATH=$(dirname "$0")
 alloc="def-ilafores"
 email="false"
 walltime="24:00:00"
-threads="24"
-mem="30G"
+threads="12"
+mem="25G"
 log="false"
 
 sample_tsv="false";
