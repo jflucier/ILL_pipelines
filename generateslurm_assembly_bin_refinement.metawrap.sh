6847173be1bcd7ef1cecc73c2989eeecee9d60a6
jflucier <3505568+jflucier@users.noreply.github.com>
2023-04-20 09:23:35 -0400

    change metawrap default cpu and mem

--- generateslurm_assembly_bin_refinement.metawrap.sh
+++ generateslurm_assembly_bin_refinement.metawrap.sh
@@ -72,7 +72,7 @@ help_message () {
   echo "	--slurm_log STR	slurm log file output directory (default to output_dir/logs)"
   echo "	--slurm_email \"your@email.com\"	Slurm email setting"
   echo "	--slurm_walltime STR	slurm requested walltime (default 24:00:00)"
-  echo "	--slurm_threads INT	slurm requested number of threads (default 48)"
+  echo "	--slurm_threads INT	slurm requested number of threads (default 24)"
   echo "	--slurm_mem STR	slurm requested memory (default 251G)"
 
   echo ""
@@ -90,7 +90,7 @@ alloc="def-ilafores"
 email="false"
 walltime="24:00:00"
 threads="24"
-mem="30G"
+mem="125G"
 log="false"
 
 sample_tsv="false";
